80bac7bef5f71dc6d4d40ca9fea246eec6dddc97
Oleg Berlo <oleg.b@oxtech.org>
2023-04-15 13:06:38 +0300

    Added 3D Canvas

---
+++ src/canvas/Shirt.jsx
@@ -0,0 +1,27 @@
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import { Decal, useGLTF, useTexture } from '@react-three/drei';
+
+import state from '../store';
+const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('./shirt_baked.glb');
+
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+
+  return (
+    <group>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        materiaL={materials.lambert1}
+        materiaL-roughness={1}
+        dispose={null}
+      ></mesh>
+    </group>
+  );
+};
+
+export default Shirt;
